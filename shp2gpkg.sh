9b04929009e2d6b09e93d155b56d5be8562f06bd
Peter Hicks <peter.hicks@opentraintimes.com>
2023-01-17 13:38:47 +0000

    Update shp2gpkg script to use python3/pip3 specifically

--- shp2gpkg.sh
+++ shp2gpkg.sh
@@ -2,20 +2,20 @@
 
 if [ ! -d venv ]; then
     echo Set up python3 virtual environment
-    python -m venv venv
+    python3 -m venv venv
     source venv/bin/activate
-    pip install --upgrade pip
-    pip install -r requirements.txt
+    pip3 install --upgrade pip
+    pip3 install -r requirements.txt
 else
     source venv/bin/activate
 fi
 
 if [ ! -s network-model.gpkg ]; then
     echo Create network model GeoPKG
-    python shp2gpkg.py network-model.gpkg network-model/
+    python3 shp2gpkg.py network-model.gpkg network-model/
 fi
 if [ ! -s organisational-boundaries.gpkg ]; then
     echo Create organisational boundaries GeoPKG
-    python shp2gpkg.py organisational-boundaries.gpkg organisational-boundaries/
+    python3 shp2gpkg.py organisational-boundaries.gpkg organisational-boundaries/
 fi
 
